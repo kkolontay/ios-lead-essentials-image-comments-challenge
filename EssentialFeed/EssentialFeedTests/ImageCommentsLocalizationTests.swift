d47a1a4315cbaf2d7180c9e6b0060822e01ac2d2
Konstantin Kolontay <kkolontay@gmail.com>
2021-08-25 21:25:04 -0400

    start working with presentation module

---
+++ EssentialFeed/EssentialFeedTests/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,17 @@
+//	
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+
+  func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+    let table = "Feed"
+    let bundle = Bundle(for: FeedPresenter.self)
+
+    assertLocalizedKeyAndValuesExist(in: bundle, table)
+  }
+
+}
