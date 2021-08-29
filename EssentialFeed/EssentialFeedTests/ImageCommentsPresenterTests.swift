084c234f26b256d89746a9902c630f1145385f58
Konstantin Kolontay <kkolontay@gmail.com>
2021-08-29 14:29:28 -0400

    image comments presentation done

--- EssentialFeed/EssentialFeedTests/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageCommentsPresenterTests.swift
@@ -10,6 +10,27 @@ class ImageCommentsPresenterTests: XCTestCase {
 		XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
 	}
 
+	func test_map_createViewModel() {
+		let now = Date()
+		let calendar = Calendar(identifier: .gregorian)
+		let locale = Locale(identifier: "en_US_POSIX")
+		let comments = [
+			ImageComment(id: UUID(), message: "a message", createdAt: now.adding(minutes: -5, calendar: calendar), username: "a username"),
+			ImageComment(id: UUID(), message: "another message", createdAt: now.adding(days: -1, calendar: calendar), username: "another username")
+		]
+
+		let viewModel = ImageCommentsPresenter.map(comments, currentDate: now, calendar: calendar, locale: locale)
+
+		XCTAssertEqual(viewModel.comments, [
+			ImageCommentViewModel(
+				message: "a message",
+				username: "a username", date: "5 minutes ago"),
+			ImageCommentViewModel(
+				message: "another message",
+				username: "another username", date: "1 day ago")
+		])
+	}
+
 	// MARK: - Helpers
 
 	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
