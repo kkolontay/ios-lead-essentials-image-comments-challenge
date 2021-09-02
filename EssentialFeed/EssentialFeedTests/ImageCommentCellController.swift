c74ae84b115fcd723e4ab3dd99f254f1670511c0
Konstantin Kolontay <kkolontay@gmail.com>
2021-09-02 18:40:40 -0400

    added test with extra large font

--- EssentialFeed/EssentialFeedTests/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeedTests/ImageCommentCellController.swift
@@ -6,7 +6,6 @@ import UIKit
 import EssentialFeed
 
 public final class ImageCommentCellController: NSObject {
-	public typealias ResourceViewModel = ImageCommentViewModel
 	private let viewModel: ImageCommentViewModel
 	private var cell: ImageCommentCell?
 
@@ -15,7 +14,7 @@ public final class ImageCommentCellController: NSObject {
 	}
 }
 
-extension ImageCommentCellController: UITableViewDataSource, UITableViewDelegate, UITableViewDataSourcePrefetching {
+extension ImageCommentCellController: UITableViewDataSource {
 	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		1
 	}
@@ -28,30 +27,4 @@ extension ImageCommentCellController: UITableViewDataSource, UITableViewDelegate
 
 		return cell!
 	}
-
-	public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
-
-	public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {}
-
-	public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {}
-
-	public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
-		cancelLoad()
-	}
-
-	private func cancelLoad() {
-		releaseCellForReuse()
-	}
-
-	private func releaseCellForReuse() {
-		cell = nil
-	}
-}
-
-extension ImageCommentCellController: ResourceView, ResourceLoadingView, ResourceErrorView {
-	public func display(_ viewModel: ImageCommentViewModel) {}
-
-	public func display(_ viewModel: ResourceLoadingViewModel) {}
-
-	public func display(_ viewModel: ResourceErrorViewModel) {}
 }
